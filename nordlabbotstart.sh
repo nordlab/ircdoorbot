f5876ec234622af846440c0055bdb61dd0d3e48f
root <root@cubietruck.nordlab>
2016-08-15 20:52:04 +0000

    Improve start script by adding output logging

--- nordlabbotstart.sh
+++ nordlabbotstart.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+logfile=ircdoorbot.log
+
 cd /root/ircdoorbot
-until python ircdoorbot.py; do
-    echo "Server 'myserver' crashed with exit code $?. Respawning..." >&2
+
+# Append date to logfile to see when it was started
+date -Iseconds >> $logfile
+
+while true; do
+    # Use -u to use unbuffered (raw) output (needed for logfile!)
+    python -u ircdoorbot.py
+    echo "Script crashed with exit code $?. Respawning..." >&1
     sleep 30
-done
+done >> $logfile 2>&1
