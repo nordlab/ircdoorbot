fd72a57bf708752fe995bf477388c207fe53fbb0
horstfffl <horsthorsthorst@hmamail.com>
2018-12-29 15:01:08 +0100

    disable logging and add checkout path

--- nordlabbotstart.sh
+++ nordlabbotstart.sh
@@ -2,14 +2,15 @@
 
 logfile=ircdoorbot.log
 
-cd /root/ircdoorbot
+cd /home/pi/doorbot/ircdoorbot/
 
 # Append date to logfile to see when it was started
-date -Iseconds >> $logfile
+date -Iseconds
 
 while true; do
     # Use -u to use unbuffered (raw) output (needed for logfile!)
-    python -u ircdoorbot.py
+    python3 -u ircdoorbot.py
     echo "Script crashed with exit code $?. Respawning..." >&1
     sleep 30
-done >> $logfile 2>&1
+#done >> $logfile 2>&1
+done
